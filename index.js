6c7ae4437d0699a26119617378f5494fb4c12e3c
Aditya Deepak <deepak2@purdue.edu>
2017-11-20 20:39:58 -0500

    Error corrected

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function capitalizeFirstLetter(string) {
 }
 
 app.post("/ai", (req, res) => {
-    if (req.body.result.action === 'weather' && req.body.parameters['date'].length == 0) {
+    if (req.body.result.action === 'weather' && req.body.result.parameters['date'].length == 0) {
         let city = req.body.result.parameters['geo-city']
         city = city.toString().replace(' ', '+')
         let restUrl = 'https://api.openweathermap.org/data/2.5/weather?APPID=62c3e8807b031a9af517a8208bee4328&q=' + city;
@@ -63,7 +63,7 @@ app.post("/ai", (req, res) => {
 
             callSendAPI(sender_psid, response);
         });
-    } else if (req.body.result.action === 'weather' && req.body.parameters.date.length != 0) {
+    } else if (req.body.result.action === 'weather' && req.body.result.parameters.date.length != 0) {
         let city = req.body.result.parameters['geo-city'];
         let date = req.body.result.parameters['date'];
         city = city.replace(' ', '+');
